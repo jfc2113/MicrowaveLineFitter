d777c288d0c384a93408f80f0b12255746d85da1
jfc2113 <jfc2113@gmail.com>
2015-05-04 15:11:24 -0400

    Update LineFind.py

--- LineFind.py
+++ LineFind.py
@@ -3,20 +3,20 @@ from math import *
 
 
 
-cutchans=5		##This might work
+cutchans=5		
 print "Importing LineFind.  Cutchans = ", cutchans
 
 def sd_MAD(yar):
 	med = np.median(yar)
 	return 1.4826*np.median(np.abs(yar-med))
 
-def cut(xar,yar,cutSig,Set_rms = 'dummy'):
+def cut(xar,yar,cutSig,Set_rms = 'dummy',startCR=30750.,endCR=49150.,sep=1850.):
 	fullcutX = np.zeros((2))
 	fullcutY = np.zeros((2))
 	rmss=[]
 	iii=0
-	for freqCenter in np.arange(30750.,50000.,1850.):
-		xmask = abs(xar-freqCenter)<925.
+	for freqCenter in np.arange(startCR,endCR+0.2*sep,sep):
+		xmask = abs(xar-freqCenter)<sep/2.
 		xnow = xar[xmask]
 		ynow = yar[xmask]
 		if len(xnow) ==0:
@@ -71,7 +71,7 @@ def neighborCut(xar,xar_old,yar,yar_old,cc): 	#cc is the number of channels you 
 		mainii+=1	
 	return xar, yar
 
-def spect(infile):
+def spect(infile,units='MHz'):
 	t=open(infile)
 	freq=[]
 	fluxD=[]
@@ -81,20 +81,23 @@ def spect(infile):
 		try: a=float(l[0])
 		except:	continue
 		b=float(l[1])
-		freq.append(a)
+		if units=='MHz':
+			freq.append(a)
+		if units=='GHz':
+			freq.append(a*1000)
 		fluxD.append(b)
 	t.close()
 	return np.asarray(freq),np.asarray(fluxD)
 
 
 
-def LineFind(f,fd,r_rms='dummy',plotQ='False',cc1=cutchans,rmsthresh0=4,rmsthresh1=3.0):
+def LineFind(f,fd,r_rms='dummy',plotQ='False',cc1=cutchans,rmsthresh0=4,rmsthresh1=3.5,startCR=30750,endCR=49150,sep=1850):
 	fLF=f	#frequency, line free
 	fdLF=fd	#flux dens, line free
-	fLF,fdLF,rmsfinal=cut(fLF,fdLF,rmsthresh0,r_rms)		##Do this to evaluate the standard deviation using the Median Absolute Deviation.
+	fLF,fdLF,rmsfinal=cut(fLF,fdLF,rmsthresh0,r_rms,startCR=30750,endCR=49150,sep=1850)		##Do this to evaluate the standard deviation using the Median Absolute Deviation.
 	fLF,fdLF=neighborCut(fLF,f,fdLF,fd,2)		##Cut 2 channels before evaluating the standard deviation again.
-	fLF,fdLF,rmsff=cut(fLF,fdLF,rmsthresh1,r_rms)		##Evaluate the standard deviation using only the non-line data.
-	fLF,fdLF,rmsfinal=cut(f,fd,rmsthresh1,rmsff)		##Apply the cut to the original data using the calculated rms from the non-line data
+	fLF,fdLF,rmsff=cut(fLF,fdLF,rmsthresh1,r_rms,startCR=30750,endCR=49150,sep=1850)		##Evaluate the standard deviation using only the non-line data.
+	fLF,fdLF,rmsfinal=cut(f,fd,rmsthresh1,rmsff,startCR=30750,endCR=49150,sep=1850)		##Apply the cut to the original data using the calculated rms from the non-line data
 	fLF,fdLF=neighborCut(fLF,f,fdLF,fd,cc1)	##Neighbor cut!
  	freq_diff = []
  	fd_diff = []
